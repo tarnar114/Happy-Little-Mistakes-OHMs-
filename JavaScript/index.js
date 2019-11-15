e4dfff3964069bb6cd0662654e05b4d43f29db70
user.email : tiwaojo@gmail.com <tiwaojo@gmail.com>
2019-11-15 11:47:07 -0500

    Navbar Updated

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,12 @@
 //test
 
 // This isnt a test its a drill
+
+function on() {
+    document.getElementById("searchOverlay").style.display = "block";
+    // document.getElementById("searchOverlay").
+  }
+  
+  function off() {
+    document.getElementById("searchOverlay").style.display = "none";
+  }
