8346d9ea9ec98c2f97a5d258088f49179605ef06
user.email : tiwaojo@gmail.com <tiwaojo@gmail.com>
2019-11-18 09:00:36 -0500

    Homepage and Navbar Updated

--- PHP/homePage.php
+++ PHP/homePage.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include_once '../HTML/nav.html';?>
+<?php include_once '../HTML/nav.html'; ?>
 
 <body>
-    <div class="container py-5">
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div></div>
+  <div class="container py-5 ">
+    <div class="jumbotron">
+      <div class="container share">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></div>
+      <img src="../Images/o_A8fBmg.jpeg" alt="bobross" style="max-width:50%;max-height=50%;">
+    </div>
+    
+  </div>
 </body>
+
 </html>
