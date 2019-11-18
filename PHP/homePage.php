ac08e9a29a5fbbe09a4ffa0e389814d90ae1c3c9
user.email : tiwaojo@gmail.com <tiwaojo@gmail.com>
2019-11-18 13:04:01 -0500

    Homepage and Navbar Updated

--- PHP/homePage.php
+++ PHP/homePage.php
@@ -7,10 +7,15 @@
   <div class="container py-5 ">
     <div class="jumbotron">
       <div class="container share">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h1 class="display-4 text-right">Hello, world!</h1>
+
+        <blockquote class="blockquote text-right">
+          <h2 class="display-10 mb-0">We don't make mistakes, just happy little accidents.”</h2>
+          <footer class="blockquote-footer">Bob Ross</footer>
+        </blockquote>
+        <!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> -->
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="text-right">"Talent is a pursued interest. Anything that you're willing to practice, you can do.”</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
       <img src="../Images/o_A8fBmg.jpeg" alt="bobross" style="max-width:50%;max-height=50%;">
