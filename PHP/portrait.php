083efdb5ae94a18169f24c75d76747683ff96149
camerondaize <57687569+camerondaize@users.noreply.github.com>
2019-11-19 17:56:00 -0500

    asd

---
+++ PHP/portrait.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../HTML/nav.html'; ?>
+
+<style>
+<?php include '../CSS/masonry.css'; ?>
+</style>
+
+<body>
+
+<h1>Portrait</h1>
+
+<div class="container masonry">
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Cameron</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Cameron</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Tiwa</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Deepak</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Tiwa</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@John</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Tanzir</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Tiwa</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+
+<a href="../PHP/trending.php">
+  <div class="item">
+   <p>@Tanzir</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Chris</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+  
+<a href="../PHP/trending.php">
+  <div class="item">
+    <p>@Cameron</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+
+<a href="../PHP/trending.php">
+    <div class="item">
+    <p>@Deepak</p>
+    <img src="../Images/o_A8fBmg.jpeg">
+  </div>
+</a>
+
+</div>
+
+
+</body>
+<?php include_once '../HTML/foot.html'; ?>
+</html>
