6a52add2fc074bd198fdbcebbeac2a2c258187ed
camerondaize <57687569+camerondaize@users.noreply.github.com>
2019-11-19 17:44:51 -0500

    sasd

--- PHP/trending.php
+++ PHP/trending.php
@@ -11,54 +11,90 @@
 <h1>Trending Now!</h1>
 
 <div class="container masonry">
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Cameron</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Cameron</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Tiwa</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Deepak</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Tiwa</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@John</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Tanzir</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Tiwa</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+
+<a href="../PHP/trending.php">
   <div class="item">
    <p>@Tanzir</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Chris</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+  
+<a href="../PHP/trending.php">
   <div class="item">
     <p>@Cameron</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+
+<a href="../PHP/trending.php">
     <div class="item">
     <p>@Deepak</p>
     <img src="../Images/o_A8fBmg.jpeg">
   </div>
+</a>
+
 </div>
 
 
