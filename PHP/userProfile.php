adb980f790e25ee74c1c283232e426963427001a
user.email : tiwaojo@gmail.com <tiwaojo@gmail.com>
2019-11-24 14:47:52 -0500

    User and Search Updated

---
+++ PHP/userProfile.php
@@ -0,0 +1,46 @@
+
+<?php include '../HTML/nav.html'; ?>
+<?php
+
+// if(!empty($_GET)){
+
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "assignment3";
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// if(isset($_GET['username'])){
+//     $username=$_GET['username'];
+//     $password=$_POST['password'];
+// }
+// $searchFor='%' . $_GET['searchSubmit']. '%';
+
+// WHERE username '".$username."'LIMIT 1";
+$sql = "SELECT * FROM users WHERE Id=['Id']";
+
+$result = mysqli_query($connection, $sql);
+?>
+<?php
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $fname = $row['FirstName'];
+        $lname = $row['LastName'];
+        $uname = $row['Username'];
+        $email = $row['EmailAddress'];
+        $id = $row["Id"];
+        echo "'<h1> Name: " . $fname . " " . $lname . '<br>' . " Username: " . $uname . "<br>" . " Email: " . $email . "</h1>'";
+    }
+}
+
+mysqli_close($connection);
+// $statement= $pdo->prepare($sql);
+// $statement->bindValue(1,$searchFor);
+// $statement->execute();
+
+
+
+// }
