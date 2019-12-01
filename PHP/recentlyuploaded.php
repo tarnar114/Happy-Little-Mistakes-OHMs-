6f811be465cfaa07cc5e39096893468225f6fe41
camerondaize <57687569+camerondaize@users.noreply.github.com>
2019-12-01 15:50:46 -0500

    asd

--- PHP/recentlyuploaded.php
+++ PHP/recentlyuploaded.php
@@ -1,13 +1,22 @@
+<!-- 
+    Objectives:
+               Create a homepage for the website. This page will include a navigation bar, allowing the user to direct the website at ease.
+               Create a voting poll, where the user can vote on their favorite image from a select amount of images.
+               Create a welcoming jumbotron for the website.
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
+
+<!-- Including the navagation bar at the top of the homepage -->
 <?php include '../HTML/nav.php'; ?>
 
 <style>
+/* Including the style provided by the css masonry.css */
 <?php include '../CSS/masonry.css'; ?>
 </style>
 
 <body>
-
 <div style="height:50px"></div>
     <div class="container ">
       <div class="row">
