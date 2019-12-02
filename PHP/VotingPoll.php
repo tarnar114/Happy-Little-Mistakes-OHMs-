c9a4fd66848c9498097c600f1da8cf2d2bbda277
camerondaize <57687569+camerondaize@users.noreply.github.com>
2019-12-01 22:25:46 -0500

    votingpoll.php comments

--- PHP/VotingPoll.php
+++ PHP/VotingPoll.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- Creating a PHP voting poll with AJAX -->
 <?php
+// Getting content from the voting.txt file, which stores all voting variables
 $vote = $_REQUEST['vote'];
 $nameoffile = "voting.txt";
+// Placing the content in an array within the text document
 $content = file($nameoffile);
-$array = explode("||", $content[0]);
+$array = explode("||", $content[0]); // Explode() allows us to break a string into an array, initially a seperator in the array
+// Creating 8 variables in the array
 $photo1 = $array[0];
 $photo2 = $array[1];
 $photo3 = $array[2];
@@ -15,6 +18,7 @@ $photo6 = $array[5];
 $photo7 = $array[6];
 $photo8 = $array[7];
 
+// If statements to change the array if a certain vote is selected
 if ($vote == 0) {
   $photo1 = $photo1 + 1;
 }
@@ -39,15 +43,15 @@ if ($vote == 6) {
 if ($vote == 7) {
   $photo8 = $photo8 + 1;
 }
-
+// Inserting the votes into the text file
 $updatevote = $photo1 . "||" . $photo2 . "||" . $photo3 . "||" . $photo4 . "||" . $photo5 . "||" . $photo6 . "||" . $photo7 . "||" . $photo8;
-$finalize = fopen($nameoffile, "w");
-fputs($finalize, $updatevote);
-fclose($finalize);
+$finalize = fopen($nameoffile, "w"); // Opens the file
+fputs($finalize, $updatevote); // An alias to fwrite(), binary safe file writing 
+fclose($finalize); // Closes an open file pointer, finalizes the changes to the array in text file
 ?>
 
 <body>
-
+<!-- Outputting the final values of the voting after the user has voted -->
   <style>
     /* Including the style provided by the css masonry.css */
     <?php include '../CSS/masonry.css'; ?>
